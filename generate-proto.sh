9c413a75092bf51b8a30bdf16d5597282870ad12
Florian Papsdorf <florian.papsdorf@student.hpi.de>
2022-05-19 14:26:38 +0200

    Modify proto script for v2

--- generate-proto.sh
+++ generate-proto.sh
@@ -2,3 +2,4 @@
 
 protoc --proto_path=proto --python_out=build/gen proto/bakdata/corporate/v1/corporate.proto
 protoc --proto_path=proto --python_out=build/gen proto/bakdata/lobbyist/v1/lobbyist.proto
+protoc --proto_path=proto --python_out=build/gen proto/bakdata/lobbyist/v2/lobbyist.proto
