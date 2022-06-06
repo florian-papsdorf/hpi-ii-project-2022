5085397362251dc092432c2ddf7971a21d174412
Florian Papsdorf <florian.papsdorf@student.hpi.de>
2022-06-06 15:42:20 +0200

    Extract information form corporate information field and push to new topic

--- generate-proto.sh
+++ generate-proto.sh
@@ -3,3 +3,4 @@
 protoc --proto_path=proto --python_out=build/gen proto/bakdata/corporate/v1/corporate.proto
 protoc --proto_path=proto --python_out=build/gen proto/bakdata/lobbyist/v1/lobbyist.proto
 protoc --proto_path=proto --python_out=build/gen proto/bakdata/lobbyist/v2/lobbyist.proto
+protoc --proto_path=proto --python_out=build/gen proto/bakdata/corporate_detailed/v1/corporate_detailed.proto
--- rb_transformer/rb_transformer.py
+++ rb_transformer/rb_transformer.py
@@ -1,15 +1,20 @@
 import logging
+import os
 
 from project_utilities.message_stakeholder import MessageStakeholder
-from project_utilities.constant import CORPORATE_TOPIC
+from project_utilities.constant import CORPORATE_TOPIC, CORPORATE_DETAILED_TOPIC
 from build.gen.bakdata.corporate.v1.corporate_pb2 import Corporate
+from build.gen.bakdata.corporate_detailed.v1.corporate_detailed_pb2 import Corporate_Detailed
 from project_utilities.generic_project_consumer import GenericProjectConsumer
+from project_utilities.generic_project_producer import GenericProjectProducer
 
+logging.basicConfig(
+    level=os.environ.get("LOGLEVEL", "INFO"), format="%(asctime)s | %(name)s | %(levelname)s | %(message)s"
+)
 log = logging.getLogger(__name__)
 
 
 class RbTransformer(MessageStakeholder):
-
     class RbConsumer(GenericProjectConsumer):
         def __init__(self, message_stakeholder):
             super().__init__(message_stakeholder)
@@ -21,16 +26,67 @@ class RbTransformer(MessageStakeholder):
                     continue
                 self.message_stakeholder.handle_message(msg)
 
+    class RbDetailedProducer(GenericProjectProducer):
+        def __init__(self):
+            super().__init__(Corporate_Detailed, CORPORATE_DETAILED_TOPIC)
+
+        def produce_to_topic(self, message: Corporate_Detailed):
+            self.producer.produce(
+                topic=CORPORATE_DETAILED_TOPIC, partition=-1, key=str(message.id), value=message,
+                on_delivery=self.delivery_report
+            )
+            # It is a naive approach to flush after each produce this can be optimised
+            self.producer.poll()
+
     def __init__(self):
         super(RbTransformer, self).__init__(topic=CORPORATE_TOPIC, group_id="rb_transformer", message_schema=Corporate)
         self.rb_consumer = self.RbConsumer(self)
+        self.producer = self.RbDetailedProducer()
         self.rb_consumer.consume()
+
+    @staticmethod
+    def extract_company_name(information_field: str):
+        # parse by <ref_number>: <corporate_name>,<city>,<address>,<zip_code>,<city>
+        index_end_ref_number = information_field.find(":")
+        index_end_corporate_name = information_field.find(",")
+        return information_field[index_end_ref_number + 1:index_end_corporate_name]
+
+    @staticmethod
+    def extract_persons(information_field: str, persons: list):
+        person_indicators = ["Geschäftsführer", "Einzelprokura", "Prokura erloschen", "Liquidator", "(Vorsitzender)",
+                             "Vorsitzender", "Leitungsorgan", "Inhaber", "Gesellschafter"]
+        indices = list(
+            filter(lambda x: x >= 0, list(map(lambda token: information_field.find(token), person_indicators))))
+        if len(indices) == 0:
+            return persons
+        important_end = information_field[min(indices):]
+        important_end = important_end[important_end.find(":"):]
+        name_parts = list()
+        for _ in range(0, 2):
+            delimiter = important_end.find(",")
+            name_parts.append(important_end[1:delimiter])
+            important_end = important_end[delimiter + 1:]
+        person_name = " ".join(name_parts)
+        if person_name[0] == " ":
+            person_name = person_name[1:]
+        if len(person_name) < 25:
+            persons.append(person_name)
+        return RbTransformer.extract_persons(important_end, persons)
 
     def handle_message(self, message):
         corporate = message.value()
-        print(corporate.information)
+        detailed_corporate = Corporate_Detailed()
+        detailed_corporate.id = corporate.id
+        detailed_corporate.rb_id = corporate.rb_id
+        detailed_corporate.state = corporate.state
+        detailed_corporate.company_name = RbTransformer.extract_company_name(corporate.information)
+        detailed_corporate.event_date = corporate.event_date
+        detailed_corporate.event_type = corporate.event_type
+        detailed_corporate.status = corporate.status
+        detailed_corporate.information = corporate.information
+        detailed_corporate.persons.extend(RbTransformer.extract_persons(corporate.information, list()))
+        self.producer.produce_to_topic(detailed_corporate)
 
 
 if __name__ == '__main__':
     sample_rb_transformer = RbTransformer()
-
