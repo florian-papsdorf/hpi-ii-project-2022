a6ca750ce1d30b50983c0cafd19d213810d594e7
Florian Papsdorf <florian.papsdorf@student.hpi.de>
2022-06-30 14:50:11 +0200

    Initial elasticsearch client

---
+++ project_matcher/main.py
@@ -0,0 +1,5 @@
+from project_matcher.matcher import Matcher
+
+if __name__ == "__main__":
+    sample_matcher = Matcher()
+    sample_matcher.match()
---
+++ project_matcher/matcher.py
@@ -0,0 +1,27 @@
+from elasticsearch import Elasticsearch
+
+
+class Matcher:
+    def __init__(self):
+        self.es_client = Elasticsearch()
+
+    def iterate_over_all_fold_outs(self):
+        offset = 0
+        pagesize = 5
+        while True:
+            result = self.es_client.search(index="fold_out-events", body={
+                "size": pagesize,
+                "from": offset
+            })
+            hits = result["hits"]["hits"]
+            # Stop after no more docs
+            if not hits:
+                break
+            # Yield each entry
+            yield from (hit['_source'] for hit in hits)
+            # Continue from there
+            offset += pagesize
+
+    def match(self):
+        for hit in self.iterate_over_all_fold_outs():
+            print(hit)
